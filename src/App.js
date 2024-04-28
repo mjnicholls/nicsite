57e4ca798c9ad1887f8cbd336882755cd710ef51
mark-nicholls-uncover <mark@spctr.co.uk>
2024-04-28 19:51:08 +0100

    Merge changes from origin/gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./Home/Home";
 import Approach from "./Approach/Approach";
@@ -7,6 +7,9 @@ import AboutMe from "./About/AboutMe";
 import Cost from './Cost/Cost'
 import Contact from "./Contact/Contact";
 import Privacy from "./Privacy/Privacy";
+import { Box } from '@mui/material';
+import { MainText } from "./globalStyles";
+import { Text } from "./About/Styles";
 
 const containerStyle = {
   display: "flex",
@@ -28,12 +31,40 @@ function App() {
               <Route path="/cost" element={<Cost />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/privacy" element={<Privacy />} />
-              <Route path="*" element={<Navigate to="/home" replace />} />
+              <Route path="*" element={<NoMatch />} />
             </Routes>
           </Layout>
         </div>
       </Suspense>
     </Router>
+  );
+}
+
+// Component for handling 404 errors
+function NoMatch() {
+  const location = useLocation();
+
+  // You can customize the message or UI here
+  return (
+    <Box sx={{ pl: { sm: 12, xs: 3 }, pr: { sm: 12, xs: 2}, pb: { sm: 4, xs: 2 }} }>
+
+    <Box sx={{ display: "flex", justifyContent: "center", pb: { sm: 4, xs: 2 }}} pt={4}>
+    
+            <Text 
+            sx={{ fontSize: { sm: "30px", xs: "15px" } }}
+            >
+            Oops!
+            </Text>
+    
+    </Box>
+    
+    <Box sx={{ backgroundColor: "#daffd5", borderRadius: "15px", display: "flex", justifyContent: "center" }} p={4} mb={4}>
+    <MainText pb={2} sx={{ fontSize: { xs: "12px", sm: "24px" } }}>
+    <h3>The page <code>{location.pathname}</code> does not exist. Have a lovely afternoon.</h3>
+    </MainText>
+    </Box>
+    </Box>
+    
   );
 }
 
